054b8d8ff3ad6d3a4033ffb772bd6b50364a9e91
Kim <yongseong97@naver.com>
2021-01-03 21:57:56 +0900

    first

---
+++ app/src/main/java/com/example/workspace1/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.workspace1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void liveSearchRank(View v)
+    {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://datalab.naver.com/keyword/realtimeList.naver"));
+        startActivity(intent);
+    }
+}
