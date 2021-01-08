82d8f6a8e5d07bf5c5ed2ed163bfcbdb2b42f522
Kim <yongseong97@naver.com>
2021-01-08 15:28:03 +0900

    ys drawer

--- app/src/main/java/com/example/workspace1/MainActivity.java
+++ app/src/main/java/com/example/workspace1/MainActivity.java
@@ -5,7 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +18,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById((R.id.toolbar));
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        //왜 toolbar.에서 찾아줘야되나??
+        toolbar.findViewById(R.id.menuButton).setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer);
+
+                 if(!drawer.isDrawerOpen(Gravity.LEFT))
+                     drawer.openDrawer(Gravity.LEFT);
+             }
+
+         });
+
+
 
     }
 
