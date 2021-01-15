d5659944d0daad6388286238adb75a1b11b683b9
Kim <yongseong97@naver.com>
2021-01-15 20:14:32 +0900

    ys_01_15

--- app/src/main/java/com/example/workspace1/MainActivity.java
+++ app/src/main/java/com/example/workspace1/MainActivity.java
@@ -1,42 +1,225 @@
 package com.example.workspace1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-
+import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.*;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.util.FusedLocationSource;
+import com.naver.maps.map.widget.CompassView;
+import com.naver.maps.map.widget.LocationButtonView;
+import com.naver.maps.map.widget.ScaleBarView;
+import com.naver.maps.map.widget.ZoomControlView;
+
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 100;
+    private Context context = this;
+    Fragment MapNaver, Account, Favorite, Feed, Popular, Setting;
+    Toolbar toolbar;
+    private FusedLocationSource mLocationSource;
+    private NaverMap mNaverMap;
+
+
+//    private static final String[] PERMISSIONS = {
+//            Manifest.permission.ACCESS_FINE_LOCATION,
+//            Manifest.permission.ACCESS_COARSE_LOCATION
+//    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // inflater
 
-        Toolbar toolbar = (Toolbar) findViewById((R.id.toolbar));
+        Intent intent = new Intent(this, LoadingActivity.class);
+        startActivity(intent);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer);
+
+        toolbar = (Toolbar) findViewById((R.id.toolbar));
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);//toolbar 보여주기
 
-        //왜 toolbar.에서 찾아줘야되나??
+
         toolbar.findViewById(R.id.menuButton).setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View v) {
-                 DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer);
-
                  if(!drawer.isDrawerOpen(Gravity.LEFT))
                      drawer.openDrawer(Gravity.LEFT);
              }
 
          });
 
+        MapNaver = new MapNaver();
+        Account = new Account();
+        Favorite = new Favorite();
+        Feed = new Feed();
+        Popular = new Popular();
+        Setting = new Setting();
+
+        FragmentManager fm = getSupportFragmentManager();
+        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            fm.beginTransaction().add(R.id.container, mapFragment).commit();
+        }
+
+        mapFragment.getMapAsync(this);
+        mLocationSource =
+                new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
+
+
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                drawer.closeDrawers();
+
+                int id = item.getItemId();
+
+                if(id == R.id.map)
+                {
+                    onChangedFragemnt(1,null);
+                    Toast.makeText(context, " 지도 정보를 확인합니다.", Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.account)
+                {
+                    onChangedFragemnt(2,null);
+                    Toast.makeText(context, " 계정 정보를 확인합니다.", Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.feed)
+                {
+                    onChangedFragemnt(3,null);
+                    Toast.makeText(context, " 현재피드를 확인합니다.", Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.popular)
+                {
+                    onChangedFragemnt(4,null);
+                    Toast.makeText(context, " 인기글을 확인합니다.", Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.favorite)
+                {
+                    onChangedFragemnt(5,null);
+                    Toast.makeText(context, "즐겨찾기를 확인합니다.", Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.setting)
+                {
+                    onChangedFragemnt(6,null);
+                    Toast.makeText(context, " 설정을 확인합니다.", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
 
 
     }
 
+
+    public void onChangedFragemnt(int position,Bundle bundle)
+    {
+                Fragment fragment = null;
+
+                switch (position){
+                    case 1:
+                        FragmentManager fm = getSupportFragmentManager();
+                        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map);
+                        if (mapFragment == null) {
+                            Log.d("dd","dd");
+                            mapFragment = MapFragment.newInstance();
+                        }
+
+                        mapFragment.getMapAsync(this);
+
+                        fragment = mapFragment;
+
+                        break;
+                    case 2:
+                        fragment = Account;
+
+                    break;
+                    case 3:
+                        fragment = Favorite;
+
+                        break;
+                    case 4:
+                        fragment = Feed;
+
+                        break;
+                    case 5:
+                        fragment = Popular;
+
+                        break;
+                    case 6:
+                        fragment = Setting;
+
+                        break;
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        mNaverMap = naverMap;
+        mNaverMap.setLocationSource(mLocationSource);
+        UiSettings uiSettings = mNaverMap.getUiSettings();
+        uiSettings.setZoomControlEnabled(true); // 기본값 : true
+        uiSettings.setLocationButtonEnabled(true); // 기본값 : false
+        mNaverMap.setLocationTrackingMode(LocationTrackingMode.NoFollow);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,  @NonNull int[] grantResults) {
+        if (mLocationSource.onRequestPermissionsResult(
+                requestCode, permissions, grantResults)) {
+            if (!mLocationSource.isActivated()) { // 권한 거부됨
+                mNaverMap.setLocationTrackingMode(LocationTrackingMode.None);
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(
+                requestCode, permissions, grantResults);
+    }
+//    @Override
+//    public void onMapReady(@NonNull NaverMap naverMap) {
+//
+//        Log.d("여기","여기");
+//        Marker marker = new Marker();
+//        marker.setPosition(new LatLng(35.5670135, 126.9783740));
+//        marker.setMap(naverMap);
+//
+//
+//
+//
+//        mNaverMap = naverMap;
+//        mNaverMap.setLocationSource(mLocationSource);
+//
+//        UiSettings uiSettings = mNaverMap.getUiSettings();
+//
+//        uiSettings.setScaleBarEnabled(true);
+//        uiSettings.setCompassEnabled(true);
+//
+//    }
 }
---
+++ app/src/main/java/com/example/workspace1/MapNaver.java
@@ -0,0 +1,75 @@
+package com.example.workspace1;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.UiSettings;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MapNaver#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MapNaver extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+    public MapNaver() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment NaverMap.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MapNaver newInstance(String param1, String param2) {
+        MapNaver fragment = new MapNaver();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ViewGroup rootView = (ViewGroup) getLayoutInflater().inflate(R.layout.fragment_naver_map, container, false);
+        return rootView;
+    }
+
+}
