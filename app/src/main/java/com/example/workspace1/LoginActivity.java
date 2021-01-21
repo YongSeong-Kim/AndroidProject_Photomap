7d00afd19d3842011d852649c65dbfae30a0d331
Kim Yong Seong <yongseong97@naver.com>
2021-01-21 11:05:12 +0900

    01-21

--- app/src/main/java/com/example/workspace1/LoginActivity.java
+++ app/src/main/java/com/example/workspace1/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.*;
 
@@ -17,8 +18,11 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginActivity extends AppCompatActivity {
-    private EditText et_id, et_pass;
+    private EditText l_id, l_pass;
     private Button btn_login, btn_register;
     private String username, password;
     private RequestQueue queue;
@@ -28,10 +32,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        et_id = findViewById(R.id.et_id);
-        et_pass = findViewById(R.id.et_pass);
-        btn_login = findViewById(R.id.btn_login);
-        btn_register = findViewById(R.id.btn_register);
+        l_id = findViewById(R.id.l_id);
+        l_pass = findViewById(R.id.l_pass);
+        btn_login = findViewById(R.id.l_btn_login);
+        btn_register = findViewById(R.id.l_btn_register);
 
         queue = Volley.newRequestQueue(this);
 
@@ -47,87 +51,58 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-//                String url = "";
-//
-//                JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, url, null, new Response.Listener<JSONObject>() {
-//                    @Override
-//                    public void onResponse(JSONObject response) {
-//
-//                        try {
-//                            username = response.getString("username");
-//                            password = response.getString("password");
-//                            boolean success = response.getBoolean("success");
-//                            if (success) { // 로그인에 성공한 경우
-//                                String userID = response.getString("userID");
-//                                String userPass = response.getString("userPassword");
-//
-//                                Toast.makeText(getApplicationContext(),"로그인에 성공하였습니다.",Toast.LENGTH_SHORT).show();
-//                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-////                                intent.putExtra("userID", userID);
-////                                intent.putExtra("userPass", userPass);
-//                                startActivity(intent);
-//                            } else { // 로그인에 실패한 경우
-//                                Toast.makeText(getApplicationContext(),"로그인에 실패하였습니다.",Toast.LENGTH_SHORT).show();
-//                                return;
-//                            }
-//
-//                        } catch (JSONException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//
-//                    }, new Response.ErrorListener() {
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {
-//
-//                    }
-//                });
-//
-//                queue.add(jsonRequest);
-
-                Toast.makeText(getApplicationContext(),"로그인에 성공하였습니다.",Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-
-                startActivity(intent);
-
-//                // EditText에 현재 입력되어있는 값을 get(가져온다)해온다.
-//                String userID = et_id.getText().toString();
 //                String userPass = et_pass.getText().toString();
-//
-//                Response.Listener<String> responseListener = new Response.Listener<String>() {
-//                    @Override
-//                    public void onResponse(String response) {
-//                        try {
-//                            // TODO : 인코딩 문제때문에 한글 DB인 경우 로그인 불가
-//                            System.out.println("hongchul" + response);
-//                            JSONObject jsonObject = new JSONObject(response);
-//                            boolean success = jsonObject.getBoolean("success");
-//                            if (success) { // 로그인에 성공한 경우
-//                                String userID = jsonObject.getString("userID");
-//                                String userPass = jsonObject.getString("userPassword");
-//
-//                                Toast.makeText(getApplicationContext(),"로그인에 성공하였습니다.",Toast.LENGTH_SHORT).show();
-//                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-//                                intent.putExtra("userID", userID);
-//                                intent.putExtra("userPass", userPass);
-//                                startActivity(intent);
-//                            } else { // 로그인에 실패한 경우
-//                                Toast.makeText(getApplicationContext(),"로그인에 실패하였습니다.",Toast.LENGTH_SHORT).show();
-//                                return;
-//                            }
-//                        } catch (JSONException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                };
+//                String email = et_email.getText().toString();
+//                String userName = et_name.getText().toString();
 
-//                LoginRequest loginRequest = new LoginRequest(userID, userPass, responseListener);
-//                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
-//                queue.add(loginRequest);
+
+                String url = "http://35.238.98.83:8000/user/login/";
+
+
+                Map<String, String> params = new HashMap<String, String>();
+                Log.d("유저네임", "들어갑니다.");
+                params.put("username", l_id.getText().toString());
+                params.put("email", "");
+                params.put("password", l_pass.getText().toString());
+                JSONObject parameters = new JSONObject(params);
+
+
+                JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, url, parameters, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        Log.d("뭐야","뭐야");
+                        try {
+                            if(response.getString("message").equals("SUCCESS"))
+                            {
+                                Toast.makeText(getApplicationContext(),"로그인에 성공하였습니다.",Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            }
+                            else { // 로그인에 실패한 경우
+                                Toast.makeText(getApplicationContext(),"로그인에 실패하였습니다.",Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+                queue.add(jsonRequest);
+
+
+//                Toast.makeText(getApplicationContext(),"로그인에 성공하였습니다.",Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
 //
-//
-//
-//
+//                startActivity(intent);
+
             }
         });
     }
--- app/src/main/java/com/example/workspace1/RegisterActivity.java
+++ app/src/main/java/com/example/workspace1/RegisterActivity.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
 import com.android.volley.*;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
@@ -21,44 +21,54 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
-    private EditText et_id, et_pass, et_name, et_age;
+    private EditText r_email, r_pass, r_name, r_age;
     private Button btn_register;
     private String username, password;
     private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        r_email = findViewById(R.id.r_email);
+        r_pass = findViewById(R.id.r_pass);
+        r_name = findViewById(R.id.r_name);
+        r_age = findViewById(R.id.r_age);
 
-        et_id = findViewById(R.id.et_id);
-        et_pass = findViewById(R.id.et_pass);
-        et_name = findViewById(R.id.et_name);
-        et_age = findViewById(R.id.et_age);
-
-        btn_register = findViewById(R.id.btn_register);
+        btn_register = findViewById(R.id.r_btn_register);
 
         queue = Volley.newRequestQueue(this);
 
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String userID = et_id.getText().toString();
-                String userPass = et_pass.getText().toString();
-                String userName = et_name.getText().toString();
-                int userAge = Integer.parseInt(et_age.getText().toString());
 
-                String url = "";
+//                String userPass = et_pass.getText().toString();
+//                String email = et_email.getText().toString();
+//                String userName = et_name.getText().toString();
+//                int userAge = Integer.parseInt(et_age.getText().toString());
 
-                JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, url, null, new Response.Listener<JSONObject>() {
+                String url = "http://35.238.98.83:8000/user/signin/";
+
+                Map<String, String> params = new HashMap<String, String>();
+                Log.d("유저네임", "들어갑니다.");
+                params.put("username", r_name.getText().toString());
+                params.put("email", r_email.getText().toString());
+                params.put("password", r_pass.getText().toString());
+                JSONObject parameters = new JSONObject(params);
+
+                JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, url, parameters, new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
 
                         try {
-                            username = response.getString("username");
-                            password = response.getString("password");
+                        if(response.getString("message").equals("SUCCESS"))
+
+                            Log.d("Debug", response.getString("message"));
                             Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                             startActivity(intent);
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -68,46 +78,13 @@ public class RegisterActivity extends AppCompatActivity {
                     public void onErrorResponse(VolleyError error) {
 
                     }
-                }) {
-                    @Override
-                    protected Map<String, String> getParams() throws AuthFailureError {
-                        Map<String,String> params = new HashMap<String,String>();
-                        params.put("username", et_id.getText().toString());
-                        params.put("password", et_pass.getText().toString());
-                        return params;
-                    }
-                };
+                });
 
                 queue.add(jsonRequest);
-//                Response.Listener<String> responseListenr = new Response.Listener<String>() {
-//                    @Override
-//                    public void onResponse(String response) {
-//                        try {
-//                            JSONObject jsonObject = new JSONObject(response);
-//                            boolean success = jsonObject.getBoolean("success");
-//                            if(success)
-//                            {
-//                                Toast.makeText(getApplicationContext(),"ㅎㅚ원등록",Toast.LENGTH_LONG).show();
-//                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-//                                startActivity(intent);
-//                            }
-//                            else{
-//                                Toast.makeText(getApplicationContext(),"실패",Toast.LENGTH_LONG).show();
-//                                return;
-//                            }
-//                        } catch (JSONException e) {
-//                            e.printStackTrace();
-//                        }
-//
-//                    }
-//                };
-
-
 
             }
         });
 
+
     }
 }
-
-
